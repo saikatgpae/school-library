1914fcdecfd6bbbc404809ed4d6acb0560bafa86
Saikat Chakraborty <saikatgpae@gmail.com>
2023-02-09 19:09:39 +0530

    Remove linters.

--- person.rb
+++ person.rb
@@ -1,26 +1,30 @@
 class Person
   require_relative 'nameable'
-  
+
   class Person < Nameable
     attr_accessor :name, :age
     attr_reader :id
-  
+
     def initialize(age, name = 'Unknown', parent_permission: true)
       super()
       @id = Random.rand(1...1000)
       @name = name
       @age = age
-  @@ -13,6 +16,10 @@ def can_use_services?
+      @parent_permission = parent_permission
+    end
+
+    def can_use_services?
       is_of_age || @parent_permission
     end
-  
+
     def correct_name
       @name
     end
-  
+
     private
-  
+
     def of_age?
       @age >= 18
+    end
   end
 end
