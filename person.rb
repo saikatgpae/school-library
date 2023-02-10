f5f8181f5fd47d4105da36aedab6016c83e710f8
Saikat Chakraborty <saikatgpae@gmail.com>
2023-02-10 09:29:05 +0530

    Remoeve the Extra person class.

--- person.rb
+++ person.rb
@@ -1,30 +1,28 @@
-class Person
-  require_relative 'nameable'
+require_relative 'nameable'
 
-  class Person < Nameable
-    attr_accessor :name, :age
-    attr_reader :id
+class Person < Nameable
+  attr_accessor :name, :age
+  attr_reader :id
 
-    def initialize(age, name = 'Unknown', parent_permission: true)
-      super()
-      @id = Random.rand(1...1000)
-      @name = name
-      @age = age
-      @parent_permission = parent_permission
-    end
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
+    @id = Random.rand(1...1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
 
-    def can_use_services?
-      is_of_age || @parent_permission
-    end
+  def can_use_services?
+    is_of_age || @parent_permission
+  end
 
-    def correct_name
-      @name
-    end
+  def correct_name
+    @name
+  end
 
-    private
+  private
 
-    def of_age?
-      @age >= 18
-    end
+  def of_age?
+    @age >= 18
   end
 end
